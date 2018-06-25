43a1e1c827ed41498bd1a42d5cf560d2f7579f9c
Alyssa Orange <heyalyssawang@gmail.com>
2018-06-24 18:33:16 -0700

    add first atom and component; remove unused files

---
+++ src/views/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../components/header";
+
+class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <Header />;
+  }
+}
+
+export default Home;
